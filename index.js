bf5c45b31f0a6714dca4fd799788c31129985139
Emiliano Rotta <emilianorottaf@gmail.com>
2024-10-24 20:56:56 -0300

    first commits

---
+++ index.js
@@ -0,0 +1,65 @@
+// console.log("Hola mundo, estoy en la clase de node")
+
+//  Crear un script que sume dos números y muestre el resultado en la terminal.
+
+// const suma = (a, b) => a + b;
+
+// var numUno = 20
+// var numDos = 6
+
+// console.log(`la suma de ${numUno} y ${numDos} es:`, suma(numUno, numDos))
+
+// function sumar (a,b){
+//     var resultado = a + b
+//     return resultado
+//     }
+
+// console.log(sumar(3,5))
+
+
+
+// Obtener la fecha y hora actual
+// const fechaActual = new Date();
+// console.log('Fecha y hora actual: ', fechaActual);
+
+// Escribir el texto en el archivo mensaje.txt
+// const fs = require('fs')
+
+// fs.writeFile('mensaje.txt', 'Este es mi primer archivo en Node.js',
+// (err) => {
+//     if(err) throw err;
+//     console.log("archivo creado y texto escrito")
+// });
+
+// const fs = require('fs')
+
+// fs.readFile('mensaje.txt', 'utf8', (err, data)=>{
+//     if(err) throw err;
+//     console.log("El contenido del archivo es: ", data)
+// })
+
+
+///////////////////////////////////////////////
+// Ejercicios individuales:
+
+// Ejercicio 1: Saludo Personalizado
+// Consigna: Crear un script que muestre un saludo personalizado en la terminal.
+// Respuesta:
+
+
+// Ejercicio 2: Crear un script de multiplicación
+// Consigna: Crear un script que multiplique dos números y muestre el resultado en la terminal.
+// Respuesta:
+
+
+// Ejercicio 3: Crear un archivo y escribir un mensaje
+// Consigna: Crear un archivo llamado notas.txt y escribir un mensaje en él. (el mensaje tiene que ser sobre 3 diferencias entre back y front)
+// Respuesta:
+
+// Ejercicio 4: Mostrar un arreglo de colores
+// Consigna: Crear un array con nombres de colores y mostrarlos en la consola. (mediante un bucle como el for)
+// Respuesta:
+
+// Ejercicio 5: Mostrar el contenido de un archivo
+// Consigna: Leer el contenido del archivo notas.txt y mostrarlo en la consola.
+// Respuesta:
